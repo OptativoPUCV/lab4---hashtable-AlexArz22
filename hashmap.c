76babfc36f41cd5eea30a7b7716511529ffc3f97
Replit user <>
2023-04-12 23:10:35 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -49,8 +49,7 @@ void insertMap(HashMap * map, char * key, void * value) {
   else{
     for(int i=(posicion+1)%map->capacity; i!=posicion%map->capacity ; i=(i+1) % map->capacity){
       if(map->buckets[i]==NULL){
-        map->buckets[i]->key=key;
-        //strcpy(map->buckets[i]->key,key);
+        strcpy(map->buckets[i]->key,key);
         map->buckets[i]->value=value;
       } 
     }
