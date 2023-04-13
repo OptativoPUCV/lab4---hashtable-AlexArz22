825253d270554a74798371c4e0a66c3a1227c8c5
Replit user <>
2023-04-13 00:35:05 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -139,9 +139,9 @@ Pair * firstMap(HashMap * map) {
 
 Pair * nextMap(HashMap * map) {
 
-  if(map->buckets[map->current+1]!=NULL){
-    if(map->buckets[map->current+1]->key!=NULL){
-      return map->buckets[map->current+1];
+  if(map->buckets[map->current]!=NULL){
+    if(map->buckets[map->current]->key!=NULL){
+      return map->buckets[map->current];
     }
   }
   for(long i=(map->current+1)%map->capacity; i!=map->current; i=(i+1)%map->capacity){
